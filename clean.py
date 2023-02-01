5ee5b3f6192dc1d43e92004a1cb17caae929a11b
Kyle Fredrickson <kyfredri@ucsc.edu>
2023-01-31 20:28:04 -0800

    Huge performance optimization.

--- clean.py
+++ clean.py
@@ -6,7 +6,6 @@ import numpy as np
 import os
 import pandas as pd
 import requests
-from tqdm import tqdm
 import zipfile
 
 # This code cleans the enron and seattle datasets.
@@ -19,29 +18,26 @@ import zipfile
 #    - Reorganizes messages so that they can be looked up per user.
 #    - Strips extraneous characters from senders and receivers for a basic approach to entity resolution.
 
-def clean_enron_sender_o2o(s):
+def download(url, file_path):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_path, "wb") as file:
+            for d in r.iter_content(chunk_size=8192):
+                file.write(d)
+
+def clean_enron(s):
     if type(s) is str:
         s = s.replace("[", "")
         s = s.replace("]", "")
         s = s.replace("\'", "")
         s = s.replace("\"", "")
         s = s.replace(" ", "")
-        s = s.replace(".", "")
 
         return s
     else:
         return ""
 
-def clean_enron_receiver_o2o(s):
-    s = clean_enron_sender_o2o(s)
-    s = s.split(",")
-
-    if len(s) != 1:
-        return ""
-    else:
-        return s[0]
-
-def clean_seattle_sender_o2o(s):
+def clean_seattle(s):
     if type(s) is str:
         s = s.split("<")[0]
         s = s.split("(")[0]
@@ -53,28 +49,16 @@ def clean_seattle_sender_o2o(s):
     else:
         return ""
 
-def clean_seattle_receiver_o2o(s):
+def clean_multiple(s, clean_fn, delimiter, single_senders):
     if type(s) is str:
-        s = s.split(";")
-        if len(s) != 1:
-            return ""
+        s = [clean_fn(r) for r in s.split(delimiter)]
+
+        if single_senders and len(s) != 1:
+            return []
         else:
-            return clean_seattle_sender_o2o(s[0])
+            return s
     else:
-        return ""
-
-def clean_enron_sender(s):
-    pass
-
-def clean_enron_receiver(s):
-    pass
-
-def clean_seattle_sender(s):
-    pass
-
-def clean_seattle_receiver(s):
-    pass
-
+        return []
 
 def convert_time(s):
     """
@@ -88,59 +72,65 @@ def convert_time(s):
     except:
         return -1
 
-def correct_o2o(df, start, end):
+def expand(row):
+    r = np.empty((len(row[1]), len(row)), dtype=object)
+    r[:] = row
+    for i in range(r.shape[0]):
+        r[i, 1] = row[1][i]
+    return r
+
+def correct(df, single_senders):
     sender_correct = df.sender.apply(lambda x: x != "" and x.lower() != "nan")
     receiver_correct = df.receiver.apply(lambda x: x != "" and x.lower() != "nan")
-    cc_correct = df.cc.apply(lambda x: type(x) is not str or x.lower() == "nan")
-    bcc_correct = df.bcc.apply(lambda x: type(x) is not str or x.lower() == "nan")
 
-    all_correct = sender_correct & receiver_correct & cc_correct & bcc_correct
+    all_correct = sender_correct & receiver_correct
+
+    if single_senders:
+        cc_correct = df.cc.apply(lambda x: type(x) is not str or x.lower() == "nan")
+        bcc_correct = df.bcc.apply(lambda x: type(x) is not str or x.lower() == "nan")
+        all_correct &= cc_correct & bcc_correct
+
     return df[all_correct]
 
-def clean_o2o(df, start, end, sender_clean, receiver_clean):
+def clean(df, start, end, clean_fn, delimiter, single_senders):
+    df.sender = df.sender.apply(clean_fn)
+    df.receiver = df.receiver.apply(lambda x: clean_multiple(x, clean_fn, delimiter, single_senders))
+
+    reorder_cols = ["sender", "receiver", "submit", "cc", "bcc"]
+    df = df[reorder_cols].to_numpy()
+    df = np.concatenate([expand(df[i, :]) for i in range(df.shape[0])], axis=0)
+    df = pd.DataFrame(df, columns=reorder_cols)
+    df = correct(df, single_senders)
     df.submit = df.submit.apply(convert_time)
     df = df[df.submit.apply(lambda x: start <= x and x <= end)]
 
-    df.sender = df.sender.apply(sender_clean)
-    df.receiver = df.receiver.apply(receiver_clean)
-    df = correct_o2o(df, start, end)
+    df.sender, senders = df.sender.factorize()
+    df.receiver, receivers = df.receiver.factorize()
 
-    return df[["sender", "receiver", "submit"]]
+    senders = pd.DataFrame(senders)
+    receivers = pd.DataFrame(receivers)
 
-def clean(df, start, end, sender_clean, receiver_clean):
-    pass
-
-def sort_matrix(m, col):
-    m = m[m[:, col].argsort()]
-    return m
+    clean_cols = ["sender", "receiver", "submit"]
+    return df[clean_cols], senders, receivers
 
 def users(df):
+    df = df[np.lexsort((df[:, 2], df[:, 0]))]
+
     users = np.unique(df[:, 0])
-    submits = np.empty(len(users), dtype=object)
     receivers = np.empty(len(users), dtype=object)
-    for i in tqdm(range(len(users))):
-        user = users[i]
-        idx = df[:, 0] == user
-        m = sort_matrix(df[idx, 1:], 1)
-        receivers[i] = m[:, 0]
-        submits[i] = m[:, 1]
-        df = np.delete(df, idx, axis=0)
+    submits = np.empty(len(users), dtype=object)
+
+    min = 0
+    for i in range(df.shape[0]):
+        if df[i, 0] != df[min, 0] or i == df.shape[0] - 1:
+            receivers[df[min, 0]] = df[min:i, 1]
+            submits[df[min, 0]] = df[min:i, 2]
+            min = i
 
     return pd.DataFrame({"sender": users, "receivers": receivers, "submits": submits})
 
-
-def download(url, file_path):
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(file_path, "wb") as file:
-            for d in r.iter_content(chunk_size=8192):
-                file.write(d)
-
-def decompress(in_path, out_path):
-    with zipfile.ZipFile(in_path, "r") as zip:
-        zip.extractall(out_path)
-
-def process(url, zip_path, clean_path, processed_path, rename, start, end, clean_fn, clean_sender_fn, clean_receiver_fn):
+def process(url, zip_path, clean_path, senders_path, receivers_path,
+            processed_path, rename, start, end, clean_sender_fn, delimiter, single_senders):
     if not os.path.exists(processed_path):
         print(f"Creating: {processed_path}...")
 
@@ -152,60 +142,59 @@ def process(url, zip_path, clean_path, processed_path, rename, start, end, clean
                 download(url, zip_path)
                 print("done.")
 
-            raw_df = pd.read_csv(zip_path, compression="zip")
+            raw_df = pd.read_csv(zip_path, usecols=rename.keys() ,compression="zip")
             raw_df.rename(columns=rename, inplace=True)
-            clean_df = clean_fn(raw_df, start, end, clean_sender_fn, clean_receiver_fn)
+            clean_df, senders, receivers = clean(raw_df, start, end, clean_sender_fn, delimiter, single_senders)
             print("done.")
 
             clean_df.to_csv(clean_path, index=False)
+            senders.to_csv(senders_path)
+            receivers.to_csv(receivers_path)
         else:
-            clean_df = pd.read_csv(clean_path, dtype={"sender": str, "submit": int})
+            clean_df = pd.read_csv(clean_path)
 
         clean_m = clean_df.to_numpy()
         processed = users(clean_m)
         print("done.")
         processed.to_json(processed_path)
 
-def main(data_path, s):
+def main(data_path, single_senders):
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+    enron_data_path = os.path.join(data_path, "enron")
+    if not os.path.exists(enron_data_path):
+        os.makedirs(enron_data_path)
+
+    seattle_data_path = os.path.join(data_path, "seattle")
+    if not os.path.exists(seattle_data_path):
+        os.makedirs(seattle_data_path)
 
     enron_raw_url = "https://files.ssrc.us/data/enron.zip"
-    enron_zip_path = os.path.join(data_path, "enron.zip")
-    enron_clean_path = os.path.join(data_path, f"enron_clean_{'s' if s else ''}.csv")
-    enron_processed_path = os.path.join(data_path, f"enron_processed_{'s' if s else ''}.csv")
-    enron_rename = {"From": "sender", "X-From": "xsender", "X-To": "xreceiver", "To": "receiver", "X-cc": "cc", "X-bcc": "bcc", "Date": "submit"}
+    enron_zip_path = os.path.join(enron_data_path, "enron.zip")
+    enron_clean_path = os.path.join(enron_data_path, f"enron_clean{'_s' if single_senders else ''}.csv")
+    enron_senders_path = os.path.join(enron_data_path, f"enron_senders{'_s' if single_senders else ''}.csv")
+    enron_receivers_path = os.path.join(enron_data_path, f"enron_receivers{'_s' if single_senders else ''}.csv")
+    enron_processed_path = os.path.join(enron_data_path, f"enron_processed{'_s' if single_senders else ''}.csv")
+    enron_rename = {"From": "sender", "To": "receiver", "X-cc": "cc", "X-bcc": "bcc", "Date": "submit"}
     enron_start = 490320000 # January 16, 1985
     enron_end = 1007337600 # December 3, 2001
 
     seattle_raw_url = "https://files.ssrc.us/data/seattle.zip"
-    seattle_zip_path = os.path.join(data_path, "seattle.zip")
-    seattle_clean_path = os.path.join(data_path, f"seattle_clean_{'s' if s else ''}.csv")
-    seattle_processed_path = os.path.join(data_path, f"seattle_processed_{'s' if s else ''}.csv")
-    seattle_rename = {"to": "receiver", "time": "submit"}
+    seattle_zip_path = os.path.join(seattle_data_path, "seattle.zip")
+    seattle_clean_path = os.path.join(seattle_data_path, f"seattle_clean{'_s' if single_senders else ''}.csv")
+    seattle_senders_path = os.path.join(seattle_data_path, f"seattle_senders{'_s' if single_senders else ''}.csv")
+    seattle_receivers_path = os.path.join(seattle_data_path, f"seattle_receivers{'_s' if single_senders else ''}.csv")
+    seattle_processed_path = os.path.join(seattle_data_path, f"seattle_processed{'_s' if single_senders else ''}.csv")
+    seattle_rename = {"sender": "sender", "to": "receiver", "cc": "cc", "bcc": "bcc", "time": "submit"}
     seattle_start = 1483228800 # January 1, 2017
     seattle_end = 1491004800 # April 1, 2017
 
-    if s: # One-to-one communications.
-        enron_clean_fn = clean_o2o
-        enron_clean_sender_fn = clean_enron_sender_o2o
-        enron_clean_receiver_fn = clean_enron_receiver_o2o
+    process(enron_raw_url, enron_zip_path, enron_clean_path, enron_senders_path, enron_receivers_path,
+            enron_processed_path, enron_rename, enron_start, enron_end, clean_enron, ",", single_senders)
 
-        seattle_clean_fn = clean_o2o
-        seattle_clean_sender_fn = clean_seattle_sender_o2o
-        seattle_clean_receiver_fn = clean_seattle_receiver_o2o
-    else: # One-to-many communications.
-        enron_clean_fn = clean
-        enron_clean_sender_fn = clean_enron_sender
-        enron_clean_receiver_fn = clean_enron_receiver
-
-        seattle_clean_fn = clean
-        seattle_clean_sender_fn = clean_seattle_sender
-        seattle_clean_receiver_fn = clean_seattle_receiver
-
-    process(enron_raw_url, enron_zip_path, enron_clean_path, enron_processed_path, enron_rename, enron_start, enron_end,
-            enron_clean_fn, enron_clean_sender_fn, enron_clean_receiver_fn)
-
-    # process(seattle_raw_url, seattle_zip_path, seattle_clean_path, seattle_processed_path, seattle_rename, seattle_start, seattle_end,
-    #         seattle_clean_fn, seattle_clean_sender_fn, seattle_clean_receiver_fn)
+    process(seattle_raw_url, seattle_zip_path, seattle_clean_path, seattle_senders_path, seattle_receivers_path,
+            seattle_processed_path, seattle_rename, seattle_start, seattle_end, clean_seattle, ";", single_senders)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Metadata Cleaner",
@@ -215,6 +204,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     data_path = os.path.abspath(args.path)
-    s = args.s
+    single_senders = args.s
 
-    main(data_path, s)
+    main(data_path, single_senders)
